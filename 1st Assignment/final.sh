ff9da8f97955d15b61798c9df8fc76fcff626882
Rafaila <t8160018@aueb.gr>
2019-04-19 21:19:09 +0300

    add files

--- 1st Assignment/final.sh
+++ 1st Assignment/final.sh
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+#!/bin/bash
+
+gcc -Wall $final.c -lpthreads && ./a.out 100 1000
+echo
+=======
 #!/bin/bash
 gcc -pthread final.c
 ./a.out 100 1000
+>>>>>>> f2530e5ca135c74885c98798fcdd967601750dd2
